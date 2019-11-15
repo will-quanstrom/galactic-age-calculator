09c4462cf991c733926e0c4adef5b1c100de6ea2
Will Quanstrom <wquanstr215@gmail.com>
2019-11-15 10:12:44 -0800

    successfully create and fail test to calculate users age on mars

--- __tests__/galacticAge.test.js
+++ __tests__/galacticAge.test.js
@@ -10,8 +10,8 @@ describe('User', () => {
     var inputUser = new User(100, 'male', 'healthy', 'america');
     expect(inputUser.venusAge()).toEqual('62');
   });
-  // test('should correctly give users age on Mercury', () => {
-  //   var inputUser = new User(100, 'male', 'healthy', 'america');
-  //   expect(inputUser.mercuryAge()).toEqual('24');
-  // });
+  test('should correctly give users age on Mars', () => {
+    var inputUser = new User(100, 'male', 'healthy', 'america');
+    expect(inputUser.marsAge()).toEqual('188');
+  });
 });
