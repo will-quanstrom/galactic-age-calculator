e648b8f421e7ba610548a41391a4cb12031cf5f4
Will Quanstrom <wquanstr215@gmail.com>
2019-11-15 14:11:20 -0800

    add some ui including a loading bar

--- src/main.js
+++ src/main.js
@@ -2,7 +2,25 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
-import { User } from './../galacticAge.js';
+import { User } from './galacticAge.js';
 
 $(document).ready(function() {
+  $('#gone').click(function(){
+    this.value = '';
+  });
+  $('.userInput').submit(function(event){
+    event.preventDefault();
+    $('.load').show();
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 40) {
+        clearInterval(id);
+      } else {
+        width += .5;
+        elem.style.width = width + '%';
+      }
+    }
+  });
 });
