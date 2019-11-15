bb4ac5cc5494c63f34a4417c3d0831bbdeffd616
Will Quanstrom <wquanstr215@gmail.com>
2019-11-15 15:52:01 -0800

    add script for if age is older than life expectancy

--- src/main.js
+++ src/main.js
@@ -11,15 +11,10 @@ $(document).ready(function() {
   $('.userInput').submit(function(event){
     event.preventDefault();
     let age = $('#gone').val();
-    console.log(age);
     let gender = $('#gender').val();
-    console.log(gender);
     let lifestyle = $('#lifestyle').val();
-    console.log(lifestyle);
     let residence = $('#location').val();
-    console.log(residence);
     let input = new User(age, gender, lifestyle, residence);
-    console.log(input);
     $('.load').show();
     var elem = document.getElementById("myBar");
     var width = 1;
@@ -27,11 +22,22 @@ $(document).ready(function() {
     function frame() {
       if (width >= 40) {
         clearInterval(id);
-        $('#earthTime').text('You have a life expectancy of '+input.earthExpectancy()+' years. That means you will be alive about '+input.timeLeft()+' more years. Have a nice life!');
+        if(input.age > input.earthExpectancy()){
+          let diff = input.age - input.earthExpectancy();
+          let mercuryDiff = diff / .24;
+          let venusDiff = diff / .62;
+          let marsDiff = diff / 1.88;
+          let jupiterDiff = diff/ 11.86;
+          $('#earthTime').text(`Wow! You outlived everybody! You are ${diff} years older than your estimated life span in earth time. That means you're ${mercuryDiff} over your average lifespan on Mercury, ${venusDiff} over on Venus, ${marsDiff} over on Mars, and only ${jupiterDiff} years over your average lifespan on Jupiter.`);
+          $('#planetRow').hide();
+        } else{
+        $('#earthTime').text('You have a life expectancy of '+input.earthExpectancy()+' years. That means you will be alive about '+input.timeLeft()+' more years. Have a nice rest of life!');
         $('#mercury').text('On Mercury, you are '+input.mercuryAge()+' solar years old and you will probably live about '+input.mercuryExpectancy()+' more solar years there.');
         $('#venus').text('On Venus, you are '+input.venusAge()+' solar years old and you will probably live about '+input.venusExpectancy()+' more solar years there.');
         $('#mars').text('On Mars, you are '+input.marsAge()+' solar years old and you will probably live about '+input.marsExpectancy()+' more solar years there.');
         $('#jupiter').text('On Jupiter, you are '+input.jupiterAge()+' solar years old and you will probably live about '+input.jupiterExpectancy()+' more solar years there.');
+        $('#planetRow').show();
+      }
       } else {
         width += .5;
         elem.style.width = width + '%';
